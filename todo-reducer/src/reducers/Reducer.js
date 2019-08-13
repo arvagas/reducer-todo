930c1d4bf035ccc509a0f26885fc4102fc7c4fb3
Arvin Agas <arvagas@gmail.com>
2019-08-13 10:48:56 -0400

    created reducer structure added initialState within file completed step 1

---
+++ todo-reducer/src/reducers/Reducer.js
@@ -0,0 +1,18 @@
+export const initialState = [{
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}]
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        // case 'add':
+        //     return 
+        // case 'toggle':
+        //     return 
+        // case 'clear':
+        //     return 
+        default:
+            return state
+    }
+}
